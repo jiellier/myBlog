c561cd3050a571d8bd369d14a52d05c0b945a011
jielly <1945436197@qq.com>
2022-08-02 17:51:12 +0800

    建立弹窗消息文件

---
+++ src/utils/showMessage.js
@@ -0,0 +1,7 @@
+/**
+ * 弹出消息
+ * @param {String} content 消息内容
+ * @param {String} type 消息类型，info error success warn
+ * @param {HTMLElement} container 容器，消息会显示到该容器的正中
+ */
+export default function (content, type, container) {}
